204b7680de3c0aa13ef4250aab1815670ef312d0
Abdulhakeem Qusay Albayati <Abdulhakeem.Q.Ali@uotechnology.edu.iq>
2023-11-15 22:25:57 +0300

    Changing on JS and HTML by Zohal

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById('second').innerHTML = "The responsibility of JS is for Zohal";
+}
+function myFunction1() {
+    document.getElementById('second').innerHTML = "";
+}
