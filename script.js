5d3bc164bfd768ed31e17c022eb0a8ea00bea02d
Abdulhakeem Qusay Albayati <Abdulhakeem.Q.Ali@uotechnology.edu.iq>
2023-11-19 22:20:00 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    document.getElementById('second').innerHTML = "The responsibility of Zohal is JS";
-}
-function myFunction1() {
-    document.getElementById('second').innerHTML = "";
-}
