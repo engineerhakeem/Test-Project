5c630df70fa1a6e13ef0bd69e477a916b5244209
engineerhakeem <Abdulhakeem.q.ali@uotechnology.edu.iq>
2023-11-19 22:40:25 +0300

    Adding 3 files of my project

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById('second').innerHTML = "The responsibility of Zohal is JS";
+}
+function myFunction1() {
+    document.getElementById('second').innerHTML = "";
+}
